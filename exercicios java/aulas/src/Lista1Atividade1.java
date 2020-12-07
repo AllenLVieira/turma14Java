161d1aa9da40fb8dbba4296444b72c0ab77bf14e
AllenLVieira <allenvieira96@gmail.com>
2020-12-07 15:43:18 -0300

    Corrigindo frases do primeiro exercício

--- exercicios java/aulas/src/Lista1Atividade1.java
+++ exercicios java/aulas/src/Lista1Atividade1.java
@@ -9,9 +9,9 @@ public class Lista1Atividade1 {
 		
 		System.out.println("Digite a quantidade de anos que você tem: ");
 		anosUsuario = sc.nextInt();
-		System.out.println("\nDigite a quantidade de meses que você tem: ");
+		System.out.println("\nDigite a quantidade de meses passaram após seu aniversário: ");
 		mesesUsuario = sc.nextInt();
-		System.out.println("\nDigite a quantidade de dias que você tem: ");
+		System.out.println("\nDigite a quantidade de dias que passaram após esses meses: ");
 		diasUsuario = sc.nextInt();
 		diasTotais = 365*anosUsuario + 30*mesesUsuario + diasUsuario;
 		System.out.println("\nVocê tem "+diasTotais+" dias de idade.");
