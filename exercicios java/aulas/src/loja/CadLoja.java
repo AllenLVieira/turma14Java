f162eb4ef32e0eae1de172aadc78af029aacd31d
AllenLVieira <allenvieira96@gmail.com>
2020-12-09 20:21:15 -0300

    Melhorando o CadLoja

--- exercicios java/aulas/src/loja/CadLoja.java
+++ exercicios java/aulas/src/loja/CadLoja.java
@@ -8,7 +8,46 @@ public class CadLoja {
 		Scanner sc = new Scanner(System.in);
 		int opcao;
 		String nome;
-		char genero;
+		char genero, continuar = 'S';
+		do {
+			menu();
+			opcao = sc.nextInt();
+			switch (opcao) {
+			case 1:
+				sc.nextLine();
+				System.out.println("Digite seu nome: ");
+				nome = sc.nextLine();
+				do {
+					System.out.println("[M] para Masculino, [F] para feminino: ");
+					genero = sc.next().toUpperCase().charAt(0);
+				} while ((genero != 'M') && (genero != 'F'));
+				System.out.printf("Seja bem vind%s, %s\n", retornaGenero(genero), nome);
+				break;
+			case 2:
+				System.out.println("WIP");
+				break;
+			case 3:
+				System.out.println("Agradecemos pelo interesse, volte sempre!");
+				break;
+			default:
+				//
+			}
+			do {
+				sc.nextLine();
+				System.out.println("Você deseja fazer outra operação S ou N");
+				continuar = sc.next().toUpperCase().charAt(0);
+			}while((continuar != 'S') && (continuar != 'N'));
+		} while (continuar != 'N');
+		sc.close();
+	}
+
+	static void linha(int tam) {
+		for (int i = 1; i <= tam; i++) {
+			System.out.print("-");
+		}
+	}
+	
+	static void menu() {
 		linha(80);
 		System.out.println("\n                               FLASH ILUMINAÇÕES");
 		System.out.println("                 FLASH ILUMINAÇÕES, O MELHOR QUE VOCÊ PODE TER");
@@ -17,39 +56,13 @@ public class CadLoja {
 		System.out.println("[2] - GERENCIAR ESTOQUE");
 		System.out.println("[3] - SAIR");
 		System.out.println("Digite a opção desejada: ");
-		opcao = sc.nextInt();
-		switch(opcao) {
-		case 1:
-			sc.nextLine();
-			System.out.println("Digite seu nome: ");
-			nome = sc.nextLine();
-			do {
-				System.out.println("[M] para Masculino, [F] para feminino: ");
-				genero = sc.nextLine().toUpperCase().charAt(0);
-			} while((genero != 'M') && (genero != 'F'));
-			System.out.printf("Seja bem vind%s, %s",retornaGenero(genero), nome);
-			break;
-		case 2:
-			System.out.println("WIP");
-			break;
-		case 3:
-			System.out.println("Agradecemos pelo interesse, volte sempre!");
-		default:
-			//
-		}
-		sc.close();
 	}
-	static void linha(int tam) {
-		for(int i = 1; i <= tam; i++) {
-			System.out.print("-");
-		}
-	}
+
 	static String retornaGenero(char g) {
 		String pronome = "";
-		if(g == 'M') {
+		if (g == 'M') {
 			pronome = "o";
-		}
-		else if(g =='F') {
+		} else if (g == 'F') {
 			pronome = "a";
 		}
 		return pronome;
