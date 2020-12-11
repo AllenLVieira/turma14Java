e1af6df0a246507313402f43e4943ac1efcd3d5a
AllenLVieira <allenvieira96@gmail.com>
2020-12-10 21:36:41 -0300

    +Carrinho de compras

--- exercicios java/aulas/src/loja/CadLoja.java
+++ exercicios java/aulas/src/loja/CadLoja.java
@@ -6,22 +6,39 @@ public class CadLoja {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int opcao;
+		int opcao, estoqueProdutos[] = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }, cod, quantidade,
+				carrinho[] = new int[10];
 		String nome;
+		String codProdutos[] = new String[10], nomeProdutos[] = new String[] { "PLAFON REDONDO", "PENDENTE REDONDO",
+				"PENDENTE REDONDO CROMADO", "ABAJUR ALUMÍNIO", "PENDENTE 10 BOLAS", "ARANDELA", "PLAFON QUADRADO",
+				"LUSTRE PRETO 3 LÂMPADAS", "LUSTRE ROSÊ 13 LÂMPADAS", "ABAJUR SIMPLES" };
+		double precoProdutos[] = new double[] { 305, 76, 80, 90, 660, 100, 80, 350, 1400, 30 }, somaTotal;
 		char genero, continuar = 'S';
+
+		for (int i = 0; i < 10; i++) {
+			if (i < 9) {
+				codProdutos[i] = "FLA-00" + (i + 1);
+			} else {
+				codProdutos[i] = "FLA-0" + (i + 1);
+			}
+		}
+
 		do {
-			menu();
+			menuInicial();
 			opcao = sc.nextInt();
 			switch (opcao) {
 			case 1:
 				sc.nextLine();
+				somaTotal = 0.0;
 				System.out.println("Digite seu nome: ");
 				nome = sc.nextLine();
 				do {
 					System.out.println("[M] para Masculino, [F] para feminino: ");
 					genero = sc.next().toUpperCase().charAt(0);
 				} while ((genero != 'M') && (genero != 'F'));
-				System.out.printf("Seja bem vind%s, %s\n", retornaGenero(genero), nome);
+				System.out.printf("Seja bem vind%s, %s\n\n", retornaGenero(genero), nome);
+				adicionarCarrinho(codProdutos, estoqueProdutos, nomeProdutos, precoProdutos, carrinho);
+				carrinhoCompras(estoqueProdutos, nomeProdutos, precoProdutos, somaTotal,carrinho);
 				break;
 			case 2:
 				System.out.println("WIP");
@@ -36,9 +53,74 @@ public class CadLoja {
 				sc.nextLine();
 				System.out.println("Você deseja fazer outra operação S ou N");
 				continuar = sc.next().toUpperCase().charAt(0);
-			}while((continuar != 'S') && (continuar != 'N'));
+			} while ((continuar != 'S') && (continuar != 'N'));
 		} while (continuar != 'N');
 		sc.close();
+	}
+
+	static void adicionarCarrinho(String codProdutos[], int estoqueProdutos[], String nomeProdutos[],
+			double precoProdutos[], int carrinho[]) {
+		char continuar;
+		int cod, quantidade;
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			carrinho[i] = 0;
+		}
+		do {
+			mostrarProdutos(codProdutos, precoProdutos, nomeProdutos);
+			System.out.print("Digite o código do produto desejado.\nFLA-0");
+			cod = sc.nextInt();
+			if (cod >= 1 && cod <= 10) {
+				System.out.println("Digite quantas unidades quer comprar: ");
+				quantidade = sc.nextInt();
+				if (estoqueProdutos[cod - 1] >= quantidade) {
+					estoqueProdutos[cod - 1] -= quantidade;
+					carrinho[cod-1] +=quantidade;
+				} else if (estoqueProdutos[cod - 1] == 0) {
+					System.out.println("Não temos mais este produto. Desculpe o incoveniente.");
+				} else {
+					System.out.println("Não temos essa quantidade para esse produto! Temos apenas "
+							+ estoqueProdutos[cod - 1] + " unidades.");
+					do {
+						System.out.println("Você gostaria de levar apenas essa quantidade (S ou N)?");
+						continuar = sc.next().toUpperCase().charAt(0);
+					} while ((continuar != 'S') && (continuar != 'N'));
+					if (continuar == 'S') {
+						carrinho[cod-1] +=estoqueProdutos[cod - 1];
+						estoqueProdutos[cod - 1] -= estoqueProdutos[cod - 1];
+					}
+				}
+			}
+			do {
+				System.out.println("Você gostaria de comprar outro produto (S ou N)?");
+				continuar = sc.next().toUpperCase().charAt(0);
+			} while ((continuar != 'S') && (continuar != 'N'));
+		} while (continuar == 'S');
+	}
+
+	static void carrinhoCompras(int estoqueProdutos[], String nomeProdutos[], double precoProdutos[],
+			double somaTotal, int carrinho[]) {
+		linha(80);
+		System.out.println("\nCarrinho de compras");
+		linha(80);
+		System.out.println();
+		for (int i = 0; i < 10; i++) {
+			if (carrinho[i] != 0) {
+				System.out.printf("%s\t\t%d\t\t%.2f\n", nomeProdutos[i], carrinho[i], precoProdutos[i]);
+				somaTotal += carrinho[i] * precoProdutos[i];
+			}
+		}
+		System.out.println("\n\nPreço total: " + somaTotal);
+	}
+
+	static void mostrarProdutos(String codProdutos[], double precoProdutos[], String nomeProdutos[]) {
+		linha(60);
+		System.out.println("\nCÓD.\t\tPREÇO\t\tNOME DO PRODUTO");
+		linha(60);
+		System.out.println();
+		for (int i = 0; i < 10; i++) {
+			System.out.printf("%s\t\t%.2f\t\t%s\n", codProdutos[i], precoProdutos[i], nomeProdutos[i]);
+		}
 	}
 
 	static void linha(int tam) {
@@ -46,8 +128,8 @@ public class CadLoja {
 			System.out.print("-");
 		}
 	}
-	
-	static void menu() {
+
+	static void menuInicial() {
 		linha(80);
 		System.out.println("\n                               FLASH ILUMINAÇÕES");
 		System.out.println("                 FLASH ILUMINAÇÕES, O MELHOR QUE VOCÊ PODE TER");
